d7f927e3dbf08c2461ec27e720f8bf7f44168425
Jaroslav Henner <jhenner@redhat.com>
2018-04-25 10:26:43 +0200

    Add support for Microsoft Ergonomic Keyboard 4000

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -37,3 +37,12 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s kralovstvi/.vimrc ~jhenner/.vimrc
 vim +PluginInstall +qall
 
+# Make sure the Microsoft Ergonomic Keyboard 4000 scrolling works.
+cat > /etc/udev/hwdb.d/61-keyboard-local.hwdb <<EOF
+# Microsoft Natural Ergonomic Keyboard 4000 - remap zoom in/out to page up/down
+evdev:input:b0003v045Ep00DB*
+ KEYBOARD_KEY_c022d=pageup
+ KEYBOARD_KEY_c022e=pagedown
+EOF
+sudo udevadm hwdb --update
+sudo udevadm control --reload
